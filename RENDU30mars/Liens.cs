a6963fd69754ac8630b21d28b5f6610c442a523d
Soso <solal.chatelain@gmail.com>
2025-04-04 10:38:23 +0200

    Ajout de commentaires (Solal)
    Commentaires sur Lien

--- RENDU30mars/Liens.cs
+++ RENDU30mars/Liens.cs
@@ -9,30 +9,30 @@ namespace RENDU30mars
 {
     public class Lien
     {
-        private Noeud noeud1;
-        private Noeud noeud2;
-        private int poids;
-
+        private Noeud noeud1; /// extrémité 1 du lien
+        private Noeud noeud2; /// extrémité 2 du lien
+        private int poids; /// valeur associée au lien (temps de trajet, distance...)
+    
         public Lien(Noeud noeud1, Noeud noeud2, int poids)
         {
             this.noeud1 = noeud1;
             this.noeud2 = noeud2;
             this.poids = poids;
         }
-
+    
         public Noeud Noeud1
         {
-            get { return this.noeud1; }
+            get { return this.noeud1; } /// getter pour la 1ère extrémité
         }
-
+    
         public Noeud Noeud2
         {
-            get { return this.noeud2; }
+            get { return this.noeud2; } /// getter pour la 2ème extrémité
         }
-
+    
         public int Poids
         {
-            get { return this.poids; }
+            get { return this.poids; } /// getter pour le poids du lien
         }
     }
 }
